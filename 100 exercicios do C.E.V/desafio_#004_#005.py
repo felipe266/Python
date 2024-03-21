8e903d81f44d80f6b28508314d41cba3428d4099
Felipe Rodrigues de Oliveira <felipexfe34@gmail.com>
2024-03-21 11:30:13 -0300

    colocando as perguntas

--- 100 exercicios do C.E.V/desafio_#004_#005.py
+++ 100 exercicios do C.E.V/desafio_#004_#005.py
@@ -1,4 +1,4 @@
-#EXERCÍCIO 4
+#EXERCÍCIO 4: Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele
 algo = input('Digite algo: ')
 print(f'qual o formato? {type(algo)}')
 print(f'é alfanumerico? {algo.isalnum()}')
@@ -11,7 +11,7 @@ print(f'a 1º letra é maiúscula? {algo.istitle()}')
 
 print('*~~'*20)
 
-#EXERCÍCIO 5
+#EXERCÍCIO 5: Faça um programa que leia um número inteiro e mostre na tela seu sucessor e antecessor
 num = int(input('DIgite um número? ').strip())
 
 antecessor = (num - 1)
