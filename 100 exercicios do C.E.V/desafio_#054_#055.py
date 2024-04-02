82adcec558765e7e11b1b26445f40281b87bc95f
Felipe Rodrigues de Oliveira <felipexfe34@gmail.com>
2024-04-02 17:28:37 -0300

    colocando strip

--- 100 exercicios do C.E.V/desafio_#054_#055.py
+++ 100 exercicios do C.E.V/desafio_#054_#055.py
@@ -15,7 +15,7 @@ print('*=='*20)
 
 #EXERCÍCIO 55: Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos.
 for n in range(0,7):
-    peso = str(input(f'qual o peso do {n+1}º Kg '))
+    peso = str(input(f'qual o peso do {n+1}º Kg ').strip())
     peso = float(peso.replace(',','.'))
     
     if n == 0:
