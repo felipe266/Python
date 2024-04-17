bd1818414d7608c08830869f8fff85d559ffbe35
Felipe Rodrigues de Oliveira <felipexfe34@gmail.com>
2024-04-17 16:25:06 -0300

    exercício 98

--- 100 exercicios do C.E.V/desafio_#097_#098.py
+++ 100 exercicios do C.E.V/desafio_#097_#098.py
@@ -10,4 +10,23 @@ escreva('ola mundo')
 escreva('Ola mestre felipe')
 
 #EXERCÍCIO 98: Faça um programa que tenha uma função chamada contador(), que receba três parâmetros: início, fim e passo. Seu programa tem que realizar três contagens através da função criada:
-# a) de 1 até 10, de 1 em 1b) de 10 até 0, de 2 em 2c) uma contagem personalizada
+# a) de 1 até 10, de 1 em 1 b) de 10 até 0, de 2 em 2c) uma contagem personalizada
+def contador(ini, fim, pas):
+    if fim >= ini:
+        for n in range(ini, fim+1,pas):
+            print(n,end=' ')
+        print()
+    elif ini >= fim:
+        for n in range(ini, fim-1, -pas):
+            print(n,end=' ')
+        print()
+
+
+print('Contagem de 1 até 10')
+contador(1, 10, 1)
+print('-='*15)
+print('contagem de 10 até 0 de 2 em 2')
+contador(10, 0, 2)
+print('-='*15)
+print('personalize sua contagem')
+contador(int(input('Início: ').strip()), int(input('Fim: ').strip()), int(input('Passo: ').strip()))
