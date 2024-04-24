56089c8c50ce9192c05e1f0c7a6b280421c5f3fe
Felipe Rodrigues de Oliveira <felipexfe34@gmail.com>
2024-04-24 15:08:26 -0300

    ajeitando

--- 100 exercicios do C.E.V/desafio_#107_#108_#109_#110.py.py
+++ 100 exercicios do C.E.V/desafio_#107_#108_#109_#110.py.py
@@ -2,8 +2,8 @@
 #EXERCÍCIO 108: Adapte o código do desafio #107, criando uma função adicional chamada moeda() que consiga mostrar os números como um valor monetário formatado.
 #EXERCÍCIO 109: Modifique as funções que form criadas no desafio 107 para que elas aceitem um parâmetro a mais, informando se o valor retornado por elas vai ser ou não formatado pela função moeda(), desenvolvida no desafio 108.
 #EXERCÍCIO 110: Adicione o módulo moeda.py criado nos desafios anteriores, uma função chamada resumo(), que mostre na tela algumas informações geradas pelas funções que já temos no módulo criado até aqui.
+#EXERCÍCIO 110: Adicione o módulo moeda.py criado nos desafios anteriores, uma função chamada resumo(), que mostre na tela algumas informações geradas pelas funções que já temos no módulo criado até aqui.
 import moeda
-
 
 p = int(input('Digite o preço: R$').strip())
 t = int(input('Digite uma taxa: ').strip())
@@ -11,11 +11,6 @@ moeda.aumentar(p,t,format=True)
 moeda.diminuir(p,t,format=True)
 moeda.dobro(p,format=False)
 moeda.metade(p,format=False)
+moeda.resumo(p,20,12)
 
 print('~='*20)
-
-#EXERCÍCIO 110: Adicione o módulo moeda.py criado nos desafios anteriores, uma função chamada resumo(), que mostre na tela algumas informações geradas pelas funções que já temos no módulo criado até aqui.
-import moeda
-
-p = int(input('digite um preço: R$').strip())
-moeda.resumo(p,20,12)
