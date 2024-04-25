9fc2c1ed107e4315eb556affa48070a016563818
Felipe Rodrigues de Oliveira <felipexfe34@gmail.com>
2024-04-25 17:26:18 -0300

    try except

--- 100 exercicios do C.E.V/desafio_#113_#114.py
+++ 100 exercicios do C.E.V/desafio_#113_#114.py
@@ -1,9 +1,9 @@
 #113: Reescreva a função leiaInt() que fizemos no desafio 104, incluindo agora a possibilidade da digitação de um número de tipo inválido. Aproveite e crie também uma função leiaFloat() com a mesma funcionalidade.
-def leiaint(msg):
+'''def leiaint(msg):
     n = str(input(msg).strip())
     while True:
         if n.isnumeric() == False:
-            print(f'\033[0;31mERRO! digite um número inteiro válido\033[m')
+            print('\033[0;31mERRO! digite um número inteiro válido\033[m')
             n = input(msg)
         else:
             break
@@ -13,16 +13,16 @@ def leiaFloat(msg):
     while True:
         n = str(input(msg).strip())
         if n.isalpha() or n == '':
-            print(f'\033[0;31mERRO! digite um número inteiro válido\033[m')
+            print('\033[0;31mERRO! digite um número inteiro válido\033[m')
         else:
             m = 0
             while True:
                 if n[m].isalpha():
-                    print(f'\033[0;31mERRO! digite um número Real válido\033[m')
+                    print('\033[0;31mERRO! digite um número Real válido\033[m')
                     n = str(input(msg))
                     m = 0
                 elif n[m] == ',':
-                    print(f'\033[0;31mERRO! troco "," por "."\033[m')
+                    print('\033[0;31mERRO! troco "," por "."\033[m')
                     n = str(input(msg))
                     m = 0
                 else:
@@ -36,7 +36,34 @@ def leiaFloat(msg):
 i = leiaint('Digite um Inteiro: ')
 r = leiaFloat('Digite um Real: ')
 
-print(f'O valor inteiro digitado foi {i} e o real foi {r}')
+print(f'O valor inteiro digitado foi {i} e o real foi {r}')'''
+
+print('-='*12)
+
+#FAZENDO COM TRY E except:
+def erroint(msg):
+    while True:
+        try:
+            n = int(input(msg).strip())
+        except (TypeError, ValueError):
+            print('\033[0;31mERRO! digite um número inteiro válido\033[m')
+        else:
+            return n
+
+
+def errifloat(msg):
+    while True:
+        try:
+            f = float(input(msg).strip().replace(',','.'))
+        except (TypeError, ValueError):
+            print('\033[0;31mERRO! digite um número Real válido\033[m')
+        else:
+            return f
+
+
+n = erroint('Digite um intério: ')
+f = errifloat('Digite um Real: ')
+print(f'O número intéro digitado foi {n} e o Real foi {f}')
 
 print('~='*20)
 
